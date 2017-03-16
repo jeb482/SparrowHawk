bfe67d6420ca709974ceb50b326ce3f92fb4393e
Jimmy <jeb482@cornell.edu>
2017-03-16 14:56:48 -0400

    Added some tests

---
+++ SparrowHawkTests/UtilTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SparrowHawk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparrowHawk.Tests
+{
+    [TestClass()]
+    public class UtilTests
+    {
+        [TestMethod()]
+        public void addSceneNodePrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+
+    [TestMethod]
+}
